3db4854ac14560dc39f1a43fb9b33697f81460b5
Maksim Gavrilyuk <macsy.gavr.job@gmail.com>
2026-05-19 12:22:13 +0400

    feat: плавность перехода между страницами

--- src/components/PageWrapper/PageWrapper.tsx
+++ src/components/PageWrapper/PageWrapper.tsx
@@ -1,8 +1,21 @@
-import React, { FC, PropsWithChildren } from "react";
+import React, { FC, PropsWithChildren, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import css from "./index.module.css";
 
 const PageWrapper: FC<PropsWithChildren> = ({ children }) => {
-  return <div className={css.container}>{children}</div>;
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+
+  return (
+    <div className={css.container}>
+      <div key={pathname} className={css.pageContent}>
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default PageWrapper;
